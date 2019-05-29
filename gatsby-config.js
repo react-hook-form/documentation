6ee7a570ec1e69527e1526b447a121ebc5461b76
Beier Luo <bluebill1049@hotmail.com>
2019-05-29 21:02:08 +1000

    fix styling and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `react-hook-form`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -25,7 +25,7 @@ const Menu = styled.aside`
 
       & > li {
         line-height: 22px;
-        padding-bottom: 15px;
+        padding-bottom: 10px;
         font-size: 18px;
         display: flex;
 
