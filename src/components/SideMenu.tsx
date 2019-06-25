ca03f430a152a8a6ddee81a00108f638a9343ea4
bill <bluebill1049@hotmail.com>
2019-06-25 20:11:08 +1000

    fix padding bottom for side nav

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -25,7 +25,7 @@ const Menu = styled.aside`
 
       & > li {
         line-height: 22px;
-        padding-bottom: 12px;
+        padding-bottom: 10px;
         font-size: 16px;
         display: flex;
 
