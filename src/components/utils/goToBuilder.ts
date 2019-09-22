f1682f01e841ad103cede8ac700bac0293fd4b11
Beier Luo <bluebill1049@hotmail.com>
2019-09-22 12:14:21 +1000

    fix link on home page

--- src/components/utils/goToBuilder.ts
+++ src/components/utils/goToBuilder.ts
@@ -1,4 +1,4 @@
 export default function goToBuilder() {
   document.title = 'React hook form - Builder'
-  window.history.pushState({ page: 'React hook form - Builder' }, 'React hook form - Builder', '/builder')
+  window.history.pushState({ page: 'React hook form - Builder' }, 'React hook form - Builder', '/form-builder')
 }
