d92e8ae299e0d780da2f8413552a1d4fa16948d2
Beier Luo <bluebill1049@hotmail.com>
2019-10-12 21:49:47 +1100

    clean up builder page

--- src/pages/form-builder.tsx
+++ src/pages/form-builder.tsx
@@ -2,7 +2,6 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import BuilderPage from "../components/BuilderPage"
-import { useEffect, useState } from "react"
 
 const Api = ({
   location,
@@ -12,23 +11,13 @@ const Api = ({
     pathname: string
   }
 }) => {
-  const [isMobile, setMobile] = useState(false)
-
-  useEffect(() => {
-    setMobile(
-      typeof window === "undefined"
-        ? false
-        : window.matchMedia("(max-width: 768px)").matches
-    )
-  }, [])
-
   return (
     <Layout location={location}>
       <Seo
         title="Form Builder"
         description="GUI for building forms with validation"
       />
-      <BuilderPage isStatic isMobile={isMobile} />
+      <BuilderPage isStatic />
     </Layout>
   )
 }
