a38d27d60920fab9ceb9c1756ed93e47561fe810
bluebill1049 <bluebill1049@hotmail.com>
2019-10-14 19:47:26 +1100

    working in progress

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,9 +2,16 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage = ({
+  location,
+}: {
+  location: {
+    search: string
+    pathname: string
+  }
+}) => (
   <Layout>
-    <Seo title="404: Not found" />
+    <Seo title="404: Not found" location={location} />
     <h1>NOT FOUND</h1>
     <p>You just found a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
