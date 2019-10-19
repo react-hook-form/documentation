a56d79f782481b427d2b958ff4018be064492d35
Beier Luo <bluebill1049@hotmail.com>
2019-10-19 17:04:03 +1100

    translate chinese home page

--- src/data/home.tsx
+++ src/data/home.tsx
@@ -9,7 +9,7 @@ export default {
   description: {
     en: "React hook for form validation without the hassle",
     jp: "React hook for form validation without the hassle",
-    zh: "React hook for form validation without the hassle",
+    zh: "React hook用于表单验证，无需麻烦",
   },
   slogan: {
     en:
@@ -38,8 +38,8 @@ export default {
       description: `Form validation should be much simpler. React Hook Form will lead you to write less code and have better performance. Check out the Get Started section and learn more on the API documentation page.`,
     },
     zh: {
-      heading: "Find it useful and interesting?",
-      description: `Form validation should be much simpler. React Hook Form will lead you to write less code and have better performance. Check out the Get Started section and learn more on the API documentation page.`,
+      heading: "发现它有用和有趣?",
+      description: `表单验证应该更简单。 React钩子形式将导致您编写更少的代码并具有更好的性能。 查看"入门"部分，并在API文档页面了解更多信息。`,
     },
   },
   features: {
@@ -90,23 +90,24 @@ export default {
     zh: [
       {
         title: "开发体验",
-        description: "",
+        description:
+          "直观的功能和完整的API在构建表单时为开发人员提供最好体验。",
       },
       {
-        title: "HTML standard",
-        description: `Leverage your existing HTML markup, and start validating your forms with constraint validation API.`,
+        title: "HTML标准",
+        description: `利用现有的HTML，并开始使用约束验证API验证您的表单。`,
       },
       {
-        title: "Super Light",
-        description: `Performance is important and package size matters. it is a tiny library and without any dependencies.`,
+        title: "超轻",
+        description: `性能很重要，尺寸很重要。 它是一个很小的库，没有任何依赖关系。`,
       },
       {
-        title: "Performance",
-        description: `Minimizes the number of re-renders and faster mounting, striving to provide the best user experience.`,
+        title: "高性能",
+        description: `最大限度地减少render的数量和更快的安装，努力提供最佳的用户体验。`,
       },
       {
-        title: "Adoptable",
-        description: `Since form state is inherently local, it can be easily adopted without other dependencies.`,
+        title: "适应性强",
+        description: `由于form状态是基于本地的，因此可以轻松地在没有其他依赖关系的情况下采用它。`,
       },
     ],
   },
@@ -120,8 +121,8 @@ export default {
       description: `Reducing the amount of code that you have to write is one of the primary goals for React Hook Form. To illustrate that, let's look at a very simple form of validation among some of the most popular form validation libraries.`,
     },
     zh: {
-      title: "Library Code Comparison",
-      description: `Reducing the amount of code that you have to write is one of the primary goals for React Hook Form. To illustrate that, let's look at a very simple form of validation among some of the most popular form validation libraries.`,
+      title: "库代码比较",
+      description: `减少您必须编写的代码量是React Hook Form的主要目标之一。 为了说明这一点，让我们来看看一些最流行的表单验证库中非常简单的验证形式。`,
     },
   },
   rendering: {
@@ -136,8 +137,8 @@ export default {
       totalReRender: "Total re-renders:",
     },
     zh: {
-      title: "Reduce Rendering",
-      description: `Do you ever wonder how many component re-renders have been triggered by the user? React Hook Form embraces uncontrolled form validation to reduce unnecessary performance impact.`,
+      title: "减少Rendering",
+      description: `你有没有想过用户触发了多少组件重新renders？ React Hook Form包含不受控制的窗体验证，以减少不必要的性能影响。`,
       totalReRender: "Total re-renders:",
     },
   },
@@ -148,8 +149,9 @@ export default {
         <p>
           The following results demonstrate how long it took to render the
           components. Results are captured under a 6x CPU slow down on App start
-          with the Chrome Dev Tools' performance tab.  The running code is from the section
-          above <a href="#codeComparison">Library Code Comparison</a>.
+          with the Chrome Dev Tools' performance tab. The running code is from
+          the section above{" "}
+          <a href="#codeComparison">Library Code Comparison</a>.
         </p>
       ),
       totalMount: "No. of mount(s)",
@@ -175,8 +177,9 @@ export default {
         <p>
           The following results demonstrate how long it took to render the
           components. Results are captured under a 6x CPU slow down on App start
-          with the Chrome Dev Tools' performance tab.  The running code is from the section
-          above <a href="#codeComparison">Library Code Comparison</a>.
+          with the Chrome Dev Tools' performance tab. The running code is from
+          the section above{" "}
+          <a href="#codeComparison">Library Code Comparison</a>.
         </p>
       ),
       totalMount: "No. of mount(s)",
@@ -197,29 +200,27 @@ export default {
       ),
     },
     zh: {
-      title: "Faster Mounting",
+      title: "更快的安装",
       description: (
         <p>
-          The following results demonstrate how long it took to render the
-          components. Results are captured under a 6x CPU slow down on App start
-          with the Chrome Dev Tools' performance tab.  The running code is from the section
-          above <a href="#codeComparison">Library Code Comparison</a>.
+          以下结果说明了render组件需要多长时间。 结果在6x
+          CPU下捕获应用程序从Chrome DevTools的性能选项卡开始减速。
+          运行的代码来自上面的<a href="#codeComparison">库代码</a>比较部分。
         </p>
       ),
-      totalMount: "No. of mount(s)",
-      totalChange: "No. of committing change(s)",
-      totalTime: "Total time",
+      totalMount: "安装的次数(s)",
+      totalChange: "提交更改的次数(s)",
+      totalTime: "总时间",
       performanceTests: (
         <>
-          <span style={{ fontSize: 20 }}>⚠</span> Want to see more intense
-          performance tests?{" "}
+          希望看到更激烈的性能测试？
           <a
             href="https://github.com/bluebill1049/react-hook-form-performance-compare"
             rel="noopener noreferrer"
           >
-            Check out the result of 1000 fields within a form here
+            在这里查看表单中1000个字段的结果
           </a>
-          .
+          。
         </>
       ),
     },
@@ -265,19 +266,16 @@ export default {
       title: "现场演示",
       description: (
         <>
-          The following form demonstrates form validation in action. Each column
-          represents what has been captured in the custom hook. You can also
-          change fields in the form by clicking the <strong>EDIT</strong>{" "}
-          button.
+          下面的表单演示了表单验证的操作。 每列表示在自定义挂接中捕获的内容。 您也可以通过单击<strong>编辑</strong>按钮来更改窗体中的字段。
         </>
       ),
-      submit: "Submit",
-      watchTitle: "Watch",
-      errorTitle: "Errors",
-      touchedTitle: "Touched",
-      watch: "Change input value to see watched values",
-      error: "Validation errors will appear here",
-      touched: "Touched fields will display here",
+      submit: "提交",
+      watchTitle: "观察",
+      errorTitle: "错误",
+      touchedTitle: "接触过的",
+      watch: "更改输入值以查看观看的值",
+      error: "验证错误将出现在这里",
+      touched: "触摸的表格将显示在这里",
     },
   },
 }
