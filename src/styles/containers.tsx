96b5bc9e3e22f495ac82b23cec62f3660d91d722
Beier Luo <bluebill1049@hotmail.com>
2019-10-26 12:51:19 +1100

    reduce max-width to improve readability

--- src/styles/containers.tsx
+++ src/styles/containers.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import breakpoints from "./breakpoints"
 
 export const Wrapper = styled.div`
-  max-width: 1440px;
+  max-width: 1200px;
   margin: 0 auto;
   overflow: hidden;
   padding: 0 15px 100px 20px;
