b20e6205ad29956abdb44a910fb8cd8c53026b93
Leah <github.leah@hrmny.sh>
2019-10-29 17:31:00 +0100

    Add missing `errors` var

--- src/components/codeExamples/accessibleCodeBase.ts
+++ src/components/codeExamples/accessibleCodeBase.ts
@@ -2,7 +2,7 @@ export default `import React from "react";
 import useForm from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => console.log(data);
 
   return (
