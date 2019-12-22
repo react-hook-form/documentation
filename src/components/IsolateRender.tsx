0e4491c8144ab8b647ad623f8c03b6ea721cbfb2
bluebill1049 <bluebill1049@hotmail.com>
2019-12-22 16:22:04 +1100

    update text

--- src/components/IsolateRender.tsx
+++ src/components/IsolateRender.tsx
@@ -2,7 +2,7 @@
 import * as React from "react"
 import styled from "styled-components"
 import { AnimateKeyframes } from "react-simple-animate"
-import { H1, Note, Title } from "../styles/typography"
+import { H1, Note } from "../styles/typography"
 import { CenterContent } from "../styles/containers"
 import { ChangeEvent, useEffect, useState } from "react"
 import { DemoForm } from "./Form"
@@ -14,17 +14,20 @@ const Wrapper = styled.div`
   grid-gap: 20px;
   margin-top: 20px;
   grid-template-columns: 1fr 1fr;
+  position: relative;
 
   @media ${breakpoints.fromMediumScreen} {
-    grid-gap: 30px;
-    grid-template-columns: 1fr 80px 1fr;
+    grid-gap: 40px;
+    grid-template-columns: 1fr 65px 1fr;
   }
 
-  & > p {
+  & p {
     display: none;
-    font-size: 40px;
-    font-weight: 200;
+    font-size: 45px;
+    font-weight: 800;
     margin-top: 160px;
+    line-height: 1;
+    background: ${colors.primary};
 
     @media ${breakpoints.fromMediumScreen} {
       display: block;
@@ -35,7 +38,9 @@ const Wrapper = styled.div`
     font-size: 14px;
 
     @media ${breakpoints.fromMediumScreen} {
-      font-size: 18px;
+      font-size: 24px;
+      font-weight: 200;
+      padding-bottom: 10px;
     }
   }
 `
@@ -46,6 +51,16 @@ const ExternalComponent = styled.div`
   padding: 10px 0;
   border-radius: 4px;
   margin: 20px 0;
+`
+
+const Line = styled.div`
+  position: absolute;
+  width: 1px;
+  background: ${colors.lightBlue};
+  height: 44%;
+  left: 50%;
+  top: 30%;
+  z-index: -1;
 `
 
 const props = {
@@ -73,6 +88,7 @@ const IsoLateInput = () => {
       play={play}
       render={({ style }) => (
         <input
+          placeholder="Type here to see result..."
           style={style}
           onChange={(e: ChangeEvent<HTMLInputElement>) => {
             updateText(e.target.value)
@@ -94,12 +110,13 @@ const ControlledInputs = () => {
   return (
     <section style={{ marginBottom: 20 }}>
       <DemoForm>
-        <Title>Controlled Form</Title>
+        <h2>Controlled Form</h2>
         <AnimateKeyframes
           keyframes={[`background: ${colors.lightBlue};`, "background: white;"]}
           play={play}
           render={({ style }) => (
             <input
+              placeholder="Type here to see result..."
               style={style}
               onChange={(e: ChangeEvent<HTMLInputElement>) => {
                 updateText(e.target.value)
@@ -158,7 +175,7 @@ export default function IsolateRender() {
       <Wrapper>
         <section>
           <DemoForm>
-            <Title>React Hook Form</Title>
+            <h2> React Hook Form</h2>
             <IsoLateInput />
             <ExternalComponent>Child Component A</ExternalComponent>
             <ExternalComponent>Child Component B</ExternalComponent>
@@ -166,7 +183,10 @@ export default function IsolateRender() {
           </DemoForm>
         </section>
 
-        <p>VS</p>
+        <div>
+          <p>VS</p>
+          <Line />
+        </div>
 
         <ControlledInputs />
       </Wrapper>
