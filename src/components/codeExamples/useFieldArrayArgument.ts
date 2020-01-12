8998655eea42d2ad34e55d7d8a9f07e63dd1fa68
bluebill1049 <bluebill1049@hotmail.com>
2020-01-12 14:36:24 +1100

    update control props as optional

--- src/components/codeExamples/useFieldArrayArgument.ts
+++ src/components/codeExamples/useFieldArrayArgument.ts
@@ -1,4 +1,4 @@
 export default `const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({ 
-  control, // control props comes from useForm 
+  control, // control props comes from useForm (optional: if you are using FormContext)
   name: 'test' // unique name for your Field Array  
 })`
