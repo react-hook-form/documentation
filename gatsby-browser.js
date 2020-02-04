77e90387000b8c2f89eff3b3fafec7b40761ab49
bluebill1049 <bluebill1049@hotmail.com>
2020-02-04 20:35:32 +1100

    update reload message

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { createStore, StateMachineProvider } from 'little-state-machine'
-import formData from './src/state/formData'
+import * as React from "react"
+import { createStore, StateMachineProvider } from "little-state-machine"
+import formData from "./src/state/formData"
 import language from "./src/state/language"
 
 createStore({
@@ -8,12 +8,14 @@ createStore({
   language,
 })
 
-export const wrapRootElement = ({ element }) => <StateMachineProvider>{element}</StateMachineProvider>
+export const wrapRootElement = ({ element }) => (
+  <StateMachineProvider>{element}</StateMachineProvider>
+)
 
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
+    `🔈 React Hook Form documentation has been updated. ` +
+      `Would you like to reload to display the latest version?`
   )
   if (answer === true) {
     window.location.reload()
