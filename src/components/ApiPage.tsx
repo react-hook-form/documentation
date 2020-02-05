8553546d5b1f887845627c6f559a7bbf7ddc3eab
Arun Kumar Mohan <arunmohandm@gmail.com>
2020-02-05 02:09:47 -0500

    Fix Advanced Usage broken link

--- src/components/ApiPage.tsx
+++ src/components/ApiPage.tsx
@@ -859,7 +859,7 @@ function ApiPage({
             <button
               className={buttonStyles.primaryButton}
               onClick={() => {
-                navigate(translateLink("advanced-usage", currentLanguage))
+                navigate(translateLink("/advanced-usage", currentLanguage))
               }}
               style={{ margin: "40px auto" }}
             >
