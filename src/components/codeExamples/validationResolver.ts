a1054f4514049ba6d4d03110ef90898a0808e2fe
Beier Luo <bluebill1049@hotmail.com>
2020-02-09 11:34:48 +1100

    update hapi code example

--- src/components/codeExamples/validationResolver.ts
+++ src/components/codeExamples/validationResolver.ts
@@ -1,38 +1,39 @@
 export default `import React from "react";
-import useForm from "react-hook-form";
-import joi from "@hapi/joi";
+import { useForm } from "react-hook-form";
+import Joi from "@hapi/joi";
 
-const schema = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required()
+const validationSchema = Joi.object({
+  username: Joi.string().required()
 });
 
 const resolver = (data: any, validationContext) => {
   const { error, value: values } = validationSchema.validate(data, {
-    abortEarly: false,
+    abortEarly: false
   });
-  
+
   return {
     values: error ? {} : values,
     errors: error
       ? error.details.reduce((previous, currentError) => {
           return {
             ...previous,
-            [currentError.path[0]]: currentError,
+            [currentError.path[0]]: currentError
           };
         }, {})
-      : {},
+      : {}
   };
 };
 
-function App() {
-  const { register, handleSubmit, errors } = useForm({ 
+export default function App() {
+  const { register, handleSubmit, errors } = useForm({
     validationResolver: resolver,
-    validationContext: { test: 'test' } 
+    validationContext: { test: "test" }
   });
-  
+
   return (
     <form onSubmit={handleSubmit(d => console.log(d))}>
       <input type="text" name="username" ref={register} />
+      <input type="submit" />
     </form>
   );
 }
