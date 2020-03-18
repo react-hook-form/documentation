659ac332889451a11d21633cb3ffbb81e93b81af
Amauri Dias <athlon.amauri@gmail.com>
2020-03-17 22:37:49 -0300

    Fixed invalid prop value, in react-native TextInput value returned by onChange must be a string

--- src/components/codeExamples/reactNativeController.ts
+++ src/components/codeExamples/reactNativeController.ts
@@ -4,12 +4,10 @@ import { useForm, Controller } from "react-hook-form";
 
 export default function App() {
   const { control, handleSubmit, errors } = useForm();
-  const onSubmit = data => Alert.alert("Form Data", data);
-  const onChange = args => {
-    return {
-      value: args[0].nativeEvent.text,
-    };
-  };
+  const onSubmit = data => Alert.alert(
+    "Form Data",
+    JSON.stringify(data),
+  );
 
   return (
     <View>
@@ -18,16 +16,23 @@ export default function App() {
         as={TextInput}
         control={control}
         name="firstName"
-        onChange={onChange}
+        onChange={args => args[0].nativeEvent.text}
         rules={{ required: true }}
         defaultValue=""
       />
       {errors.firstName && <Text>This is required.</Text>}
 
       <Text>Last name</Text>
-      <Controller as={TextInput} control={control} name="lastName" defaultValue="" />
+      <Controller
+        as={TextInput}
+        control={control}
+        name="lastName"
+        onChange={args => args[0].nativeEvent.text}
+        defaultValue=""
+      />
 
-      <Button onPress={handleSubmit(onSubmit)} />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
