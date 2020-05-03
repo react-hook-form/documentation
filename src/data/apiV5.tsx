bc67b5d5a8a9d4c1342ce15323738eb075c9914a
bluebill1049 <bluebill1049@hotmail.com>
2020-05-03 17:34:39 +1000

    fix path

--- src/data/apiV5.tsx
+++ src/data/apiV5.tsx
@@ -1,10 +1,10 @@
-import apiEn from "../data/v5/en/api"
-import apiEs from "../data/v5/es/api"
-import apiPt from "../data/v5/pt/api"
-import apiZh from "../data/v5/zh/api"
-import apiJp from "../data/v5/jp/api"
-import apiKr from "../data/v5/kr/api"
-import apiRu from "../data/v5/ru/api"
+import apiEn from "../data/V5/en/api"
+import apiEs from "../data/V5/es/api"
+import apiPt from "../data/V5/pt/api"
+import apiZh from "../data/V5/zh/api"
+import apiJp from "../data/V5/jp/api"
+import apiKr from "../data/V5/kr/api"
+import apiRu from "../data/V5/ru/api"
 
 export default {
   en: apiEn,
