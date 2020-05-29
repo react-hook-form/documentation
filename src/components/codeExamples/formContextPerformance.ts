00d02d785aee0e697939c1679a10ccd63e172640
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2020-05-29 18:17:25 +0900

    Fixed FormContext Performance code (#274)

--- src/components/codeExamples/formContextPerformance.ts
+++ src/components/codeExamples/formContextPerformance.ts
@@ -1,5 +1,23 @@
-export default `import React from "react";
+export default `import React, { memo } from "react";
 import { useForm, FormContext, useFormContext } from "react-hook-form";
+
+// we can use React.memo to prevent re-render except dirty state changed
+const NestedInput = memo(
+  ({ register, formState: { dirty } }) => (
+    <div>
+      <input name="test" ref={register} />
+      {dirty && <p>This field is dirty</p>}
+    </div>
+  ),
+  (prevProps, nextProps) =>
+    prevProps.formState.dirty === nextProps.formState.dirty
+);
+
+export const NestedInputContainer = ({ children }) => {
+  const methods = useFormContext();
+
+  return <NestedInput {...methods} />;
+};
 
 export default function App() {
   const methods = useForm();
@@ -7,31 +25,12 @@ export default function App() {
 
   return (
     <FormContext {...methods}>
-      // pass all methods into the context
+      {/* pass all methods into the context */}
       <form onSubmit={methods.handleSubmit(onSubmit)}>
-        <NestedInput />
+        <NestedInputContainer />
         <input type="submit" />
       </form>
     </FormContext>
   );
 }
-
-function NestedInput() {
-  const {
-    register,
-    formState: { dirty }
-  } = useFormContext();
-  
-  // we can use React.memo to prevent re-render except dirty state changed
-  return React.useMemo(
-    () => (
-      <div>
-        <input name="test" ref={register} />
-        {dirty && <p>This field is dirty</p>}
-      </div>
-    ),
-    [dirty]
-  );
-}
-
 `
