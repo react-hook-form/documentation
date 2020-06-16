20f0a98294b9d6d5ae5e601c32f6bb6ad199a552
Phillip Maier <pmaier983@gmail.com>
2020-06-16 13:03:05 -0400

    Add useEffect ui library effect

--- src/components/codeExamples/getStarted.ts
+++ src/components/codeExamples/getStarted.ts
@@ -196,6 +196,36 @@ export default function App() {
 }
 `
 
+export const uiLibraryTs = `import React from "react";
+import { useForm } from "react-hook-form";
+import Select from "react-select";
+import Input from "@material-ui/core/Input";
+import { Input as InputField } from "antd";
+
+interface IFormInput {
+  name: string
+}
+
+export default function App() {
+  const { register, handleSubmit, setValue } = useForm();
+  const onSubmit = data => console.log(data);
+  
+  const handleChange = (e) => {
+    setValue("AntdInput", e.target.value);
+  }
+  
+  React.useEffect(() => {
+    register("AntdInput"); // custom register Antd input
+  }, [register])
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <InputField name="name" onChange={handleChange} />
+      <input type="submit" />
+    </form>
+  );
+}`
+
 export const uiLibraryHookInput = `import React from "react";
 import Select from "react-select";
 import { useForm, Controller } from "react-hook-form";
