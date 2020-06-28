de12ebba7583c31e24f8a6985a99432d5f7f415f
Dohyung Ahn <me@rinae.dev>
2020-06-28 18:15:46 +0900

    revise korean translation - TS Support (#266)

--- src/data/kr/ts.tsx
+++ src/data/kr/ts.tsx
@@ -10,18 +10,15 @@ export default {
   resolver: {
     title: "Resolver",
     description: (
-      <p>
-        이 유형은 사용자가 사용자 정의 유효성 검증 분석기를 작성할 수 있도록
-        지원합니다.
-      </p>
+      <p>이 타입은 사용자가 커스텀 유효성 검사기를 작성할 때 유용합니다.</p>
     ),
   },
   submitHandler: {
     title: "SubmitHandler",
     description: (
       <p>
-        이 유형은 제출 된 데이터가 <code>FormValue</code> 와 일치하지 않을 때
-        유용합니다 유형.
+        이 타입은 제출 된 데이터가 <code>FormValue</code> 타입과 일치하지 않을
+        때 유용합니다.
       </p>
     ),
   },
@@ -29,8 +26,8 @@ export default {
     title: "Control",
     description: (
       <p>
-        이 유형은 <code>useFieldArray</code>, <code>useWatch</code> 및 future와
-        같은 사용자 정의 후크를 지원합니다. 이 라이브러리의 사용자 정의 후크.
+        이 타입은 <code>useFieldArray</code>, <code>useWatch</code> 및 앞으로 이
+        라이브러리에 추가될 커스텀 훅을 지원합니다.
       </p>
     ),
   },
@@ -38,9 +35,8 @@ export default {
     title: "UseFormMethodsRef",
     description: (
       <p>
-        이 유형은 컨텍스트 대신 소비자를 사용할 때 유용합니다.{" "}
-        <code>useFormContext</code>
-        후크.
+        이 타입은 <code>useFormContext</code> 훅을 사용하는 대신 직접{" "}
+        <code>Context</code> 와 <code>Consumer</code> 를 사용할 때 유용합니다.
       </p>
     ),
   },
