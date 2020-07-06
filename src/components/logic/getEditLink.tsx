f8836c1f66a9e8ef07b7f05cb97f21b6650c91c2
bluebill1049 <bluebill1049@hotmail.com>
2020-07-06 18:31:39 +1000

    update prefix

--- src/components/logic/getEditLink.tsx
+++ src/components/logic/getEditLink.tsx
@@ -1,4 +1,5 @@
-const preFix = "https://github.com/react-hook-form/documentation/edit/master/"
+const preFix =
+  "https://github.com/react-hook-form/documentation/edit/master/src/data/"
 
 export const getEditLink = (
   currentLanguage: string,
@@ -9,20 +10,20 @@ export const getEditLink = (
   if (pathname === "/" || pathname === "") {
     return preFix + "src/data/home.tsx"
   } else if (pathname.includes("get-started")) {
-    return `${preFix}src/data/${currentLanguage}/getStarted.tsx`
+    return `${preFix}${currentLanguage}/getStarted.tsx`
   } else if (pathname.includes("api")) {
-    return `${preFix}src/data/${currentLanguage}/api.tsx`
+    return `${preFix}${currentLanguage}/api.tsx`
   } else if (pathname.includes("ts")) {
-    return `${preFix}src/data/ts.tsx`
+    return `${preFix}ts.tsx`
   } else if (pathname.includes("advanced-usage")) {
-    return `${preFix}src/data/${currentLanguage}/advanced.tsx`
+    return `${preFix}${currentLanguage}/advanced.tsx`
   } else if (pathname.includes("faqs")) {
-    return `${preFix}src/data/${currentLanguage}/faq.tsx`
+    return `${preFix}${currentLanguage}/faq.tsx`
   } else if (pathname.includes("dev-tools")) {
-    return `${preFix}src/data/${currentLanguage}/devtools.tsx`
+    return `${preFix}${currentLanguage}/devtools.tsx`
   } else if (pathname.includes("form-builder")) {
-    return `${preFix}src/data/builder.tsx`
+    return `${preFix}builder.tsx`
   } else if (pathname.includes("resources")) {
-    return `${preFix}src/data/resources.tsx`
+    return `${preFix}resources.tsx`
   }
 }
