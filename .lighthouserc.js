a52a88b632e5b6722a6fb56b99cfa0da84557ace
Phillip Maier <pmaier983@gmail.com>
2020-07-14 23:52:48 -0400

    Basic lighthouse setup

---
+++ .lighthouserc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["error", {"maxNumericValue": 2500}]
+      },
+    }
+  },
+}
