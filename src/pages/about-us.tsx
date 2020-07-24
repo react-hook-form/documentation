d23d12139f4fadf2773b4a341dca37ef1ddae075
Bill <bluebill1049@hotmail.com>
2020-07-24 23:54:08 +1000

    Update src/pages/about-us.tsx
    Co-authored-by: Phillip ED Maier <pmaier983@gmail.com>

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -27,9 +27,9 @@ const data = [
     name: "Phillip ED Maier",
     imgUrl:
       "https://avatars3.githubusercontent.com/u/35811186?s=460&u=011235951b44d56d753335ac4cac32046802b300&v=4",
-    url: "",
-    bio: "xxx",
-    interests: [],
+	url: "https://phillip-maier.com/",
+    bio: "A Software Engineer with a passion for helping others 😄.",
+    interests: ["🚣 rowing", "🌳 growing trees", "🎿 skiing"],
   },
   {
     name: "Jerome De Leon",
