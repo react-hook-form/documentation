e5354c14341edd6e3c75dc10bbe107757aaf7f47
bluebill1049 <bluebill1049@hotmail.com>
2020-07-27 21:42:18 +1000

    update doc with useFieldArray example

--- src/components/codeExamples/useFieldArray.ts
+++ src/components/codeExamples/useFieldArray.ts
@@ -2,73 +2,54 @@ export default `import React from "react";
 import { useForm, useFieldArray } from "react-hook-form";
 
 function App() {
-  const { register, control, handleSubmit, reset } = useForm({
+  const { register, control, handleSubmit, reset, trigger, setError } = useForm({
     // defaultValues: {}; you can populate the fields by this attribute 
   });
-  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
-    {
-      control,
-      name: "test"
-    }
-  );
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+    control,
+    name: "test"
+  });
+  
+  // trigger validation at the field array level
+  // useEffect(() => {
+  //   if (fields) trigger('test'); // with resolver
+  //   if (!fields.length) setError('miniLength, 'at least 1 field array item'); // build-in validator
+  // }, [trigger, fields])
 
   return (
     <form onSubmit={handleSubmit(data => console.log(data))}>
       <ul>
-        {fields.map((item, index) => {
-          return (
-            <li key={item.id}>
-              <input
-                name={\`test[{\$index}].firstName\`}
-                defaultValue={item.firstName} // make sure to set up defaultValue
-                ref={register()}
-              />
+        {fields.map((item, index) => (
+          <li key={item.id}>
+            <input
+              name={\`test[{\$index}].firstName\`}
+              ref={register()}
+              defaultValue={item.firstName} // make sure to set up defaultValue
+            />
 
-              <Controller
-                as={<input />}
-                name={\`test[{\$index}].lastName\`}
-                control={control}
-                defaultValue={item.lastName} // make sure to set up defaultValue
-              />
-              <button type="button" onClick={() => remove(index)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
+            <Controller
+              as={<input />}
+              name={\`test[{\$index}].lastName\`}
+              control={control}
+              defaultValue={item.lastName} // make sure to set up defaultValue
+            />
+
+            <button type="button" onClick={() => remove(index)}>Delete</button>
+          </li>
+        ))}
       </ul>
-      <section>
-        <button
-          type="button"
-          onClick={() => {
-            append({ firstName: "appendBill", lastName: "appendLuo" });
-          }}
-        >
-          append
-        </button>
-        <button
-          type="button"
-          onClick={() =>
-            prepend({
-              firstName: "prependFirstName",
-              lastName: "prependLastName"
-            })
-          }
-        >
-          prepend
-        </button>
-        <button
-          type="button"
-          onClick={() =>
-            insert(parseInt(2, 10), {
-              firstName: "insertFirstName",
-              lastName: "insertLastName"
-            })
-          }
-        >
-          insert at
-        </button>
-      </section>
+      <button
+        type="button"
+        onClick={() => append({ firstName: "appendBill", lastName: "appendLuo" })}
+      >
+        append
+      </button>
+      <button
+        type="button"
+        onClick={() => prepend({ firstName: "prependFirstName", lastName: "prependLastName" })}
+      >
+        prepend
+      </button>
       <input type="submit" />
     </form>
   );
