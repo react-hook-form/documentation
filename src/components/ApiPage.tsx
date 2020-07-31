a34dc8e7875aef7639354645513d043b653830ea
Beier Luo <bluebill1049@hotmail.com>
2020-07-31 22:58:45 +1000

    update setValue ts

--- src/components/ApiPage.tsx
+++ src/components/ApiPage.tsx
@@ -856,8 +856,7 @@ const { register } = useForm<FormInputs>({
               <h2>
                 setValue:{" "}
                 <span className={typographyStyles.typeText}>
-                  (name: string, value: any, shouldValidate?: boolean, config:
-                  Object) =&gt; void
+                  (name: string, value: any, config: Object) =&gt; void
                 </span>
               </h2>
             </code>
