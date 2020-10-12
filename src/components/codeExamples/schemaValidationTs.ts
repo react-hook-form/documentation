5f111442412d3676179ca444fdbadb695e4ebe15
bluebill1049 <bluebill1049@hotmail.com>
2020-10-13 09:27:32 +1100

    close #447

--- src/components/codeExamples/schemaValidationTs.ts
+++ src/components/codeExamples/schemaValidationTs.ts
@@ -1,6 +1,6 @@
 export default `import React from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from '@hookform/resolvers';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 
 interface IFormInputs {
