f74c02ed4ba00d65a4f0cca250bf6aca7206bcd8
Stefano Ruth <stefanoruth@users.noreply.github.com>
2020-11-07 13:48:07 +0100

    Fix invalid example defaultValue to name

--- src/data/en/api.tsx
+++ src/data/en/api.tsx
@@ -1107,7 +1107,7 @@ React.useEffect(() => {
               rawData={`{fields.map((data, index) =>
   <input
     key={data.id}
-    defaultValue={\`data[\${index}].value\`}
+    name={\`data[\${index}].value\`}
   />;
 );}`}
               withOutCopy
