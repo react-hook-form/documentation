9b374195fe9ccecf9d15ffeaf8b1197efa1b9019
Beier Luo <bluebill1049@hotmail.com>
2020-12-09 18:47:07 +1100

    include more detail on valueAs process

--- src/components/ApiRefTable.tsx
+++ src/components/ApiRefTable.tsx
@@ -428,6 +428,10 @@ export default function ApiRefTable({
                   Returns a Number normally. If something goes wrong{" "}
                   <code>NaN</code> will be returned.
                 </p>
+                <p>
+                  <b className={typographyStyles.note}>Note</b>:{" "}
+                  <code>valueAs</code> process is happening after validation.
+                </p>
               </td>
               <td>
                 <CodeArea
@@ -457,6 +461,10 @@ export default function ApiRefTable({
                   Returns a Date normally. If something goes wrong{" "}
                   <code>null</code> will be returned.
                 </p>
+                <p>
+                  <b className={typographyStyles.note}>Note</b>:{" "}
+                  <code>valueAs</code> process is happening after validation.
+                </p>
               </td>
               <td>
                 <CodeArea
@@ -485,6 +493,10 @@ export default function ApiRefTable({
               </td>
               <td>
                 <p>Return input value by running through the function.</p>
+                <p>
+                  <b className={typographyStyles.note}>Note</b>:{" "}
+                  <code>valueAs</code> process is happening after validation.
+                </p>
               </td>
               <td>
                 <CodeArea
