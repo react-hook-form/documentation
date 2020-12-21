a4840dcb176f42fd8c7b74414a837db6b1f4647b
bluebill1049 <bluebill1049@hotmail.com>
2020-12-22 10:08:02 +1100

    fix example with formState

--- src/components/codeExamples/formState.ts
+++ src/components/codeExamples/formState.ts
@@ -2,17 +2,20 @@ export default `import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, errors, formState } = useForm();
-  const onSubmit = data => console.log(data);
-  // Read the formState before render to subscribe the form state through Proxy
-  const { isDirty, isSubmitting } = formState;
-  
+  const {
+    register,
+    handleSubmit,
+    errors,
+    // Read the formState before render to subscribe the form state through Proxy
+    formState: { isDirty, isSubmitting, touched, submitCount },
+  } = useForm();
+  const onSubmit = (data: FormInputs) => console.log(data);
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input name="test" ref={register} />
-      {isDirty && <p>Form is dirty.</p>}
-      {isSubmitting && <span>Submitting...</span>}
       <input type="submit" />
     </form>
   );
-}`
+}
+`
--- src/components/codeExamples/formStateTs.ts
+++ src/components/codeExamples/formStateTs.ts
@@ -6,15 +6,20 @@ type FormInputs = {
 }
 
 export default function App() {
-  const { register, handleSubmit, errors, formState } = useForm<FormInputs>();
+  const {
+    register,
+    handleSubmit,
+    errors,
+    // Read the formState before render to subscribe the form state through Proxy
+    formState: { isDirty, isSubmitting, touched, submitCount },
+  } = useForm<FormInputs>();
   const onSubmit = (data: FormInputs) => console.log(data);
-  // Read the formState before render to subscribe the form state through Proxy
-  const { isDirty, isSubmitting, touched, submitCount } = formState;
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input name="test" ref={register} />
       <input type="submit" />
     </form>
   );
-}`
+}
+`
