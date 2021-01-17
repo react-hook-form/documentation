532376f0b49b14183655623601d3ea9567ea8582
Slapbox <Slapbox@users.noreply.github.com>
2021-01-16 19:24:10 -0500

    Adds note about shouldUnregister: false to Modal/Tab FAQ (#504)
    * Adds note about shouldUnregister: false to Modal/Tab FAQ
    
    * Update faq.tsx
    
    Co-authored-by: Bill <bluebill1049@hotmail.com>

--- src/data/en/faq.tsx
+++ src/data/en/faq.tsx
@@ -635,6 +635,8 @@ export default {
               </a>
             </li>
           </ul>
+          
+          <p>Alternatively you can use the _deprecated_ option <code>shouldUnregister: false</code> when calling `useForm`.
         </>
       ),
     },
