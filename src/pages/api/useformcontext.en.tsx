6bba69b653beb03ab0c7e2b75f23917368bcadb7
Beier Luo <bluebill1049@hotmail.com>
2021-01-31 15:48:13 +1100

    include useFormContext

--- src/pages/api/useformcontext.en.tsx
+++ src/pages/api/useformcontext.en.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import Seo from "../../components/seo"
 import Layout from "../../components/layout"
-import UseForm from "../../components/UseForm"
+import UseFormContext from "../../components/UseFormContext"
 
 export default ({ location }: { location: any }) => {
   return (
     <Layout location={location} defaultLang="en">
       <Seo title="TS Support" location={location} />
-      <UseForm currentLanguage="en" />
+      <UseFormContext currentLanguage="en" />
     </Layout>
   )
 }
