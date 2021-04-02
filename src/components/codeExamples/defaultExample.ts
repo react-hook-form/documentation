1ba190c9827d1ac95dab36438629faa066d832a6
Moolan <ireasen_sgj@163.com>
2021-04-02 19:17:13 +0800

    Fix defaultExample code (#529)
    * Fix defaultExample code
    
    * Update defaultExample.ts
    
    Co-authored-by: xingshan <xingshan@dian.so>
    Co-authored-by: Bill <bluebill1049@hotmail.com>

--- src/components/codeExamples/defaultExample.ts
+++ src/components/codeExamples/defaultExample.ts
@@ -2,7 +2,7 @@ export default `import React from "react";
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit, watch, errors } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
 
   console.log(watch("example")); // watch input value by passing the name of it
