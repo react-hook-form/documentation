fd603c7c9d6dfb303839064ef92bc0a5a6a5a5d9
Romain Trotard <romain.trotard@gmail.com>
2021-04-02 18:18:33 +0200

    Remove brackets in default examples to be able to copy/paste it

--- src/components/codeExamples/defaultExample.ts
+++ src/components/codeExamples/defaultExample.ts
@@ -8,7 +8,7 @@ export default function App() {
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
-    {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
       <input defaultValue="test" {...register("example")} />
--- src/components/codeExamples/v6/defaultExampleTs.tsx
+++ src/components/codeExamples/v6/defaultExampleTs.tsx
@@ -13,7 +13,7 @@ export default function App() {
   console.log(watch("example")) // watch input value by passing the name of it
 
   return (
-    {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
       <input name="example" defaultValue="test" ref={register} />
