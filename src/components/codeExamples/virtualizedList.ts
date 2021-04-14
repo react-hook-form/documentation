5f4a05e5d7a7335b85f66cd3a209e679e30c2031
Beier Luo <bluebill1049@hotmail.com>
2021-04-14 16:24:27 +1000

    close #555

--- src/components/codeExamples/virtualizedList.ts
+++ src/components/codeExamples/virtualizedList.ts
@@ -1,53 +1,43 @@
-export default `import React from "react";
-import { FormContext, useForm, useFormContext } from "react-hook-form";
-import { VariableSizeList as List } from "react-window";
+export default `import React from 'react'
+import { FormProvider, useForm, useFormContext } from 'react-hook-form'
+import { VariableSizeList as List } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
+import ReactDOM from 'react-dom'
+import './styles.css'
 
 const items = Array.from(Array(1000).keys()).map((i) => ({
   title: \`List \${i}\`,
   quantity: Math.floor(Math.random() * 10),
-}));
+}))
 
-const WindowedRow = React.memo(({ index, style, data, setValue }) => {
-  const qtyKey = "[\${index}].quantity";
-  const qty = getValues()[index].quantity ?? items[index].quantity;
+const WindowedRow = React.memo(({ index, style, data }) => {
+  const { register } = useFormContext()
 
-  return (
-    <input
-      // Rather than register, we use defaultValue and setValue
-      defaultValue={qty}
-      onChange={(e) => {
-        setValue(
-          qtyKey,
-          isNaN(Number(e.target.value)) ? 0 : Number(e.target.value)
-        );
-      }}
-    />
-  );
-});
+  return <input {...register(\`\${index}.quantity\`)} />
+})
 
-export default React.memo(({ items }) => {
-  const formMethods = useForm({ defaultValues: items });
-  const onSubmit = (data) => console.log(data);
-
-  // We manually call register here for each field.
-  React.useEffect(() => {
-    items.forEach((item, idx) => {
-      formMethods.register("[\${idx}].quantity");
-    });
-  }, [formMethods, items]);
+export const App = () => {
+  const onSubmit = (data) => console.log(data)
+  const formMethods = useForm({ defaultValues: items })
 
   return (
-    <form onSubmit={formMethods.handleSubmit(onSubmit)}>
-      <List
-        itemCount={items.length}
-        itemSize={() => 50}
-        itemData={items}
-        {{ ...formMethods }}
-      >
-        {WindowedRow}
-      </List>
+    <form className="form" onSubmit={formMethods.handleSubmit(onSubmit)}>
+      <FormProvider {...formMethods}>
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              height={height}
+              itemCount={items.length}
+              itemSize={() => 100}
+              width={width}
+              itemData={items}>
+              {WindowedRow}
+            </List>
+          )}
+        </AutoSizer>
+      </FormProvider>
       <button type="submit">Submit</button>
     </form>
-  );
-});
+  )
+}
 `
