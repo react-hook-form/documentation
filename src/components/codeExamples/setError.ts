3a73333d68649ba644e7a0b0106589df6931b1b9
Beier Luo <bluebill1049@hotmail.com>
2021-04-22 15:46:01 +1000

    fix formState with setError

--- src/components/codeExamples/setError.ts
+++ src/components/codeExamples/setError.ts
@@ -2,7 +2,7 @@ export default `import React from "react";
 import { useForm } from "react-hook-form";
 
 const App = () => {
-  const { register, handleSubmit, setError, errors } = useForm();
+  const { register, handleSubmit, setError, formState: { errors } } = useForm();
   const onSubmit = data => {
     console.log(data)
   };
