6d7724beff91a42b41a8129ddc641ab69a1edcb5
GaelS <servauga@gmail.com>
2021-10-15 13:02:35 +0200

    Add `disabled` props to useWatch props signature (#678)
    According to react-form source code, `useWatch` takes the following object as argument:
    
    ```
    export type UseWatchProps<TFieldValues extends FieldValues = FieldValues> = {
      defaultValue?: unknown;
      disabled?: boolean;
      name?:
        | FieldPath<TFieldValues>
        | FieldPath<TFieldValues>[]
        | readonly FieldPath<TFieldValues>[];
      control?: Control<TFieldValues>;
    };
    ```
    but `disabled` was missing and mispelled in the documentation.

--- src/components/UseWatchContent.tsx
+++ src/components/UseWatchContent.tsx
@@ -22,7 +22,7 @@ export default function UseFieldArray({
           useWatch:{" "}
           <span
             className={typographyStyles.typeText}
-          >{`({ control?: Control, name?: string, defaultValue?: any }) => object`}</span>
+          >{`({ control?: Control, name?: string, defaultValue?: any, disabled?: boolean }) => object`}</span>
         </h2>
       </code>
 
@@ -82,7 +82,7 @@ export default function UseFieldArray({
             </tr>
             <tr>
               <td>
-                <code>disable</code>
+                <code>disabled</code>
               </td>
               <td>
                 <code className={typographyStyles.typeText}>
