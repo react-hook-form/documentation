961a9ad15cb5be5feae048595aed6a29446723de
Beier Luo <bluebill1049@hotmail.com>
2022-01-07 11:54:29 +1100

    remove misleading content

--- src/components/codeExamples/shouldUseNativeValidation.ts
+++ src/components/codeExamples/shouldUseNativeValidation.ts
@@ -5,10 +5,6 @@ export default function App() {
   const { register, handleSubmit } = useForm({ shouldUseNativeValidation: true });
   const onSubmit = async data => { console.log(data); };
 
-  // you can still disabled the native validation, CSS selectors such as
-  // invalid and valid still going to work 
-  // <form onSubmit={handleSubmit(onSubmit)} novalidate>
-
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
