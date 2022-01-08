426fc191117f074f89c9113b821e4bef0b3e7028
Beier Luo <bluebill1049@hotmail.com>
2022-01-08 11:15:13 +1100

    remove all icons

--- src/components/CodePerfCompareSection.tsx
+++ src/components/CodePerfCompareSection.tsx
@@ -27,73 +27,6 @@ function CodePerfCompareSection({
   return (
     <AnimateGroup play={isPlayRender}>
       <div className={containerStyles.centerContent}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="ionicon"
-          viewBox="0 0 512 512"
-        >
-          <title>Barbell</title>
-          <path
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-            d="M48 256h416"
-          />
-          <rect
-            x="384"
-            y="128"
-            width="32"
-            height="256"
-            rx="16"
-            ry="16"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          />
-          <rect
-            x="96"
-            y="128"
-            width="32"
-            height="256"
-            rx="16"
-            ry="16"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          />
-          <rect
-            x="32"
-            y="192"
-            width="16"
-            height="128"
-            rx="8"
-            ry="8"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          />
-          <rect
-            x="464"
-            y="192"
-            width="16"
-            height="128"
-            rx="8"
-            ry="8"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          />
-        </svg>
         <h1 className={typographyStyles.h1} id="rendering">
           {home.rendering[currentLanguage].title}
         </h1>
@@ -135,17 +68,6 @@ function CodePerfCompareSection({
       </section>
 
       <div className={containerStyles.centerContent}>
-        <svg viewBox="0 0 512 512">
-          <path
-            d="M315.27 33L96 304h128l-31.51 173.23a2.36 2.36 0 002.33 2.77h0a2.36 2.36 0 001.89-.95L416 208H288l31.66-173.25a2.45 2.45 0 00-2.44-2.75h0a2.42 2.42 0 00-1.95 1z"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="32"
-          />
-        </svg>
-
         <h1 className={typographyStyles.h1}>
           {home.mount[currentLanguage].title}
         </h1>
