5700da74f075e571ab2c39a44e17fa49c2c35cf4
Beier Luo <bluebill1049@hotmail.com>
2022-04-02 12:00:13 +1100

    fix word

--- src/components/ApiRefTable.tsx
+++ src/components/ApiRefTable.tsx
@@ -75,7 +75,7 @@ export default function ApiRefTable({
 
       <CodeArea
         rawData={`const { onChange, onBlur, name, ref } = register('firstName'); 
-// include type check again field path with the name you have supplied.
+// include type check against field path with the name you have supplied.
         
 <input 
   onChange={onChange} // assign onChange event 
