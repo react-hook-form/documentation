f6b24709e6649c4dc775910146a7a3c8e4268ba6
Beier Luo <bluebill1049@hotmail.com>
2022-04-02 14:10:20 +1100

    remove confuse example

--- src/components/codeExamples/setError.ts
+++ src/components/codeExamples/setError.ts
@@ -6,22 +6,9 @@ const App = () => {
   const onSubmit = data => {
     console.log(data)
   };
-  const { onChange, ...rest } = register("username");
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        onChange={(e) => {
-          setError("username", {
-            type: "manual",
-            message: "Dont Forget Your Username Should Be Cool!",
-          });
-          onChange(e);
-        }}
-        {...rest}
-      />
-      {errors.username && <p>{errors.username.message}</p>}
-
       <button
         type="button"
         onClick={() => {
