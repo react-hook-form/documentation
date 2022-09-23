17dc9e391b44eacf8b8b021467440634561cd0cf
Beier Luo <bluebill1049@hotmail.com>
2022-09-23 21:41:15 +1000

    update handle error section for better accessibility

--- src/components/codeExamples/getStarted.ts
+++ src/components/codeExamples/getStarted.ts
@@ -366,19 +366,21 @@ import { useForm } from "react-hook-form";
 
 export default function App() {
   const { register, formState: { errors }, handleSubmit } = useForm();
-
   const onSubmit = (data) => console.log(data);
   
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true })} />
+      <input 
+        {...register("firstName", { required: true })} 
+        aria-invalid={errors.firstName ? "true" : "false"} 
+      />
       {errors.firstName?.type === 'required' && "First name is required"}
-      
-      <input {...register("lastName", { required: true })} />
-      {errors.lastName && <p>Last name is required</p>}
 
-      <input {...register("mail", { required: "Email Address is required" })} />
-      <p>{errors.mail?.message}</p>
+      <input 
+        {...register("mail", { required: "Email Address is required" })} 
+        aria-invalid={errors.mail ? "true" : "false"} 
+      />
+      {errors.mail && <p role="alert">{errors.mail?.message}</p>}
       
       <input type="submit" />
     </form>
