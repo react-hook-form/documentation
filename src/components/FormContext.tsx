d8d445cfac432883b2beed8769f96124f2c24ab2
Beier Luo <bluebill1049@hotmail.com>
2022-09-30 12:59:13 +1000

    include new form provider page

--- src/components/FormContext.tsx
+++ src/components/FormContext.tsx
@@ -1,11 +1,10 @@
 import * as React from "react"
 import CodeArea from "./CodeArea"
 import formContext from "./codeExamples/formContext"
-import generic from "../data/generic"
 import * as typographyStyles from "../styles/typography.module.css"
-import * as tableStyles from "../styles/table.module.css"
+import { Link } from "gatsby"
 
-export default function FormContext({ currentLanguage, api }) {
+export default function FormContext({ api }) {
   return (
     <>
       <code className={typographyStyles.codeHeading}>
@@ -17,36 +16,9 @@ export default function FormContext({ currentLanguage, api }) {
 
       {api.useFormContext.introduction}
 
-      <h2 className={typographyStyles.subTitle}>Props</h2>
+      <h2 className={typographyStyles.subTitle}>Return</h2>
 
-      <p>
-        This following table applied to <code>FormProvider</code>,{" "}
-        <code>useFormContext</code> accepts no argument.
-      </p>
-
-      <div className={tableStyles.tableWrapper}>
-        <table className={tableStyles.table}>
-          <tbody>
-            <tr>
-              <th>{generic.name[currentLanguage]}</th>
-              <th>{generic.type[currentLanguage]}</th>
-              <th>{generic.description[currentLanguage]}</th>
-            </tr>
-            <tr>
-              <td>
-                <code>...props</code>
-              </td>
-              <td>
-                <code className={typographyStyles.typeText}>Object</code>
-              </td>
-              <td>
-                <code>FormProvider</code> requires all <code>useForm</code>{" "}
-                methods.
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <p>This hook will return all the useForm return methods and props.</p>
 
       {api.useFormContext.description}
 
@@ -55,8 +27,11 @@ export default function FormContext({ currentLanguage, api }) {
       </h2>
 
       <p>
-        You need to wrap your form with the <code>FormProvider</code> component
-        for <code>useFormContext</code> to work properly.
+        You need to wrap your form with the{" "}
+        <Link to="/api/formprovider">
+          <code>FormProvider</code>
+        </Link>{" "}
+        component for <code>useFormContext</code> to work properly.
       </p>
 
       <h2 id="example" className={typographyStyles.subTitle}>
