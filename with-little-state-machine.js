70a36d01e281e941f900e23d8e9591b1eef39c48
Beier Luo <bluebill1049@hotmail.com>
2022-10-08 15:23:36 +1100

    remove lang store

--- with-little-state-machine.js
+++ with-little-state-machine.js
@@ -1,12 +1,10 @@
 import * as React from "react"
 import { createStore, StateMachineProvider } from "little-state-machine"
 import formData from "./src/state/formData"
-import language from "./src/state/language"
 import setting from "./src/state/setting"
 
 createStore({
   formData,
-  language,
   setting,
 })
 
