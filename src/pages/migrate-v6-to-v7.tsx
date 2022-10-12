7ea1dd15c842d4d006fd35fabd99b87e72c5fb2b
Beier Luo <bluebill1049@hotmail.com>
2022-10-12 20:23:48 +1100

    close #844

---
+++ src/pages/migrate-v6-to-v7.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function MigrateV6ToV7() {
+  React.useEffect(() => {
+    window.location.href = "https://legacy.react-hook-form.com/migrate-v6-to-v7"
+  }, [])
+
+  return null
+}
