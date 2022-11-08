a166c94031e5581fd25c6bd5aa0a40b4c605f799
leapful <andy.tqa@gmail.com>
2022-11-08 14:54:43 +0700

    improve: Update description of "valueAsNumber" option to be used specially for <input type="number" /> (PR 9299 from react-hook-form repo) (#873)
    Co-authored-by: leapful <leapful>

--- src/components/ApiRefTable.tsx
+++ src/components/ApiRefTable.tsx
@@ -424,7 +424,7 @@ export default function ApiRefTable({
                   </li>
                   <li>
                     <p>
-                      Only applicable and support to {`<input />`}, but we still
+                      Only applicable and support to {`<input type="number" />`}, but we still
                       cast to number type without trim or any other data
                       manipulation.
                     </p>
