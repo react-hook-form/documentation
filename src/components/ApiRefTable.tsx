ddab382622e3d1a9b75c5c69b4a5478cbe771d20
Beier Luo <bluebill1049@hotmail.com>
2023-01-16 08:33:51 +1100

    update with v7.42.0

--- src/components/ApiRefTable.tsx
+++ src/components/ApiRefTable.tsx
@@ -388,11 +388,17 @@ export default function ApiRefTable({
         ? `{
       positive: v => parseInt(v) > 0,
       lessThanTen: v => parseInt(v) < 10,
+      validateNumber: (_, formValues) {
+        return formValues.number1 + formValues.number2 === 3;
+      }, 
       checkUrl: async () => await fetch(),
     }`
         : `{
       positive: v => parseInt(v) > 0 || 'should be greater than 0',
       lessThanTen: v => parseInt(v) < 10 || 'should be lower than 10',
+      validateNumber: (_: number, formValues: FormValues) {
+        return formValues.number1 + formValues.number2 === 3 || 'Check sum number';
+      },
       // you can do asynchronous validation as well
       checkUrl: async () => await fetch() || 'error message',  // JS only: <p>error message</p> TS only support string
       messages: v => !v && ['test', 'test2']
