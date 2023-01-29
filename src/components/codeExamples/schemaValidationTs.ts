da3fe72328f630888b057fafe386685ec7de02a1
Seth <seth@mckilla.dev>
2023-01-29 16:14:33 -0500

    Use yup type inference for schema validation example (#906)

--- src/components/codeExamples/schemaValidationTs.ts
+++ src/components/codeExamples/schemaValidationTs.ts
@@ -2,21 +2,17 @@ export default `import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 
-interface IFormInputs {
-  firstName: string
-  age: number
-}
-
 const schema = yup.object({
   firstName: yup.string().required(),
   age: yup.number().positive().integer().required(),
 }).required();
+type FormData = yup.InferType<typeof schema>;
 
 export default function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>({
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
     resolver: yupResolver(schema)
   });
-  const onSubmit = (data: IFormInputs) => console.log(data);
+  const onSubmit = (data: FormData) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
