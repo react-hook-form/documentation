2b86df8b439fa36f26b551a3709eccd1f7961f7c
Beier Luo <bluebill1049@hotmail.com>
2023-02-28 08:49:12 +1100

    add sponsor link for beekai

--- src/components/sponsorsList.tsx
+++ src/components/sponsorsList.tsx
@@ -16,6 +16,16 @@ export function SponsorsList() {
           <img src="/images/casinoreviews.png" alt="Casino Reviews" />
         </a>
         <a
+          href="https://www.beekai.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://www.beekai.com/marketing/logo/horizontal.svg"
+            alt="BEEKAI Form builder"
+          />
+        </a>
+        <a
           className={styles.placeholder}
           href="https://opencollective.com/react-hook-form"
           target="_blank"
