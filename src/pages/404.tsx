d9aee181f77f35e8deba44a3374a93cc67e7cad3
Sam (Unclebigbay) <ayodelesamueladebayo143@gmail.com>
2025-02-21 10:47:33 +0100

    Fix: 404 page layout (#1126)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,16 @@
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import styles from "./404.module.css"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just found a route that doesn&#39;t exist... the sadness.</p>
+    <div className={styles.container}>
+      <main className={styles.root}>
+        <h1>NOT FOUND</h1>
+        <p>You just found a route that doesn&#39;t exist... the sadness.</p>
+      </main>
+    </div>
   </Layout>
 )
 
