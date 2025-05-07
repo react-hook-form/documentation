ddf20c1ba7ef6e202603a7672142c0924976a7c9
bill <bluebill1049@hotmail.com>
2025-05-08 09:24:17 +1000

    thank you follower24 for sponsoring the project

--- src/components/sponsorsList.tsx
+++ src/components/sponsorsList.tsx
@@ -23,11 +23,11 @@ export function SponsorsList() {
         >
           <img src="/images/route4me.png" alt="route4me" />
         </a>
-        <a href="https://twicsy.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.follower24.de/" target="_blank" rel="noopener noreferrer">
           <img
             className={styles.twicsy}
-            src="/images/twicsy.png"
-            alt="twicsy"
+            src="/images/follower24.png"
+            alt="follower24"
           />
         </a>
         <a
