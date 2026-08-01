a35385b06616f90efe7c3abecfc6f8cff3ad7ab8
bill <bluebill1049@hotmail.com>
2026-08-01 11:55:48 +1000

    update since

--- src/components/Since.tsx
+++ src/components/Since.tsx
@@ -1,7 +1,12 @@
 import styles from "./Since.module.css"
 
 const Since = ({ version }: { version: string }) => {
-  return <span className={styles.since}>v{version}+</span>
+  return (
+    <span className={styles.since}>
+      <span className={styles.dot} />
+      Since <span className={styles.version}>v{version}</span>
+    </span>
+  )
 }
 
 export default Since
